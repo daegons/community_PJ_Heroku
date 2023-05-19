cf1fcaf9463517487704b452b44ea7fb73039dc0
daegons <qzom1425@naver.com>
2023-05-19 14:41:35 +0900

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,16 @@ import PostArea from "./Component/Post/PostArea";
 import MyPage from "./Component/User/MyPage";
 import MainPage from "./Component/MainPage";
 
+// //배경 영상 관련 유투브
+// import ReactPlayer from "react-player";
+
+import "./Component/assets/Landingpage.css";
+
+// import yousolo from "./Component/assets/olnyphYousolo.mp4";
+// import tv from "./Component/assets/tv.mp4";
+// import cloud from "./Component/assets/cloud.mp4";
+import moldives from "./Component/assets/moldives.mp4";
+
 function App() {
   const dispatch = useDispatch();
   // console.log(user.displayName);
@@ -47,24 +57,29 @@ function App() {
 
   return (
     <>
-      <Heading />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        {/* mongoose를 통해 post, Reple 구현 */}
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/post/:postNum" element={<PostArea />} />
-        <Route path="/edit/:postNum" element={<Edit />} />
-        {/* firebase Auth(인증)통해서 login구현 */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        {/* 
+      <div className="landingpage">
+        <video src={moldives} autoPlay muted loop class="video-bg" />
+        <div className="bg-overlay">
+          <Heading />
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            {/* mongoose를 통해 post, Reple 구현 */}
+            <Route path="/upload" element={<Upload />} />
+            <Route path="/post/:postNum" element={<PostArea />} />
+            <Route path="/edit/:postNum" element={<Edit />} />
+            {/* firebase Auth(인증)통해서 login구현 */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            {/* 
         Add
         1. 검색기능 -(post관련)
         2. 유저프로필 이미지 & 이미지 변경
         3. 정렬 최신 / 인기 - (post관련)
       */}
-        <Route path="/mypage" element={<MyPage />} />
-      </Routes>
+            <Route path="/mypage" element={<MyPage />} />
+          </Routes>
+        </div>
+      </div>
     </>
   );
 }
